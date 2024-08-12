d9641992a528ce22584280b5cd25592f53842852
aalinkil <100548229+aalinkil@users.noreply.github.com>
2024-08-12 08:32:30 -0700

    Update train.py

--- train.py
+++ train.py
@@ -20,137 +20,47 @@ from torch.utils.tensorboard import SummaryWriter
 
 from pathlib import Path
 
+## Change defaults of variables in this function
+## This function allows you to change settings in Command line interface
+def getting_vars():
 
+    ## Set if you're using your windows or mac machine (data directory location)
+    machine = 'w'     # m for mac, w for windows
 
-def get_all_sentences(ds, lang):
-    for item in ds:
-        yield item['translation'][lang]
+    ## Set if setting the model in training mode or inference mode
+    execution = 't'     # t for train, s for sample
 
-def get_or_build_tokenizer(config, ds, lang):
-    tokenizer_path = Path(config["tokenizer_file"].format(lang))
+    ## Model Variables
+    input_splits = 3
+    num_heads = 6
+    num_encoders = 
 
-    # if tokenizer does not exist, make a new one
-    if not Path.exists(tokenizer_path):
-        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
-        tokenizer.pre_tokenizer = Whitespace()
-        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency = 2)
-        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer = trainer)
-        tokenizer.save(str(tokenizer_path))
-    else:
-        tokenizer = Tokenizer.from_file(str(tokenizer_path))
-    return tokenizer
+    ## Hyperparameters
+    training_batch_size = 8
+    training_epochs = 50
+    learning_rate = 1e-3
 
-def get_ds(config):
-    ds_raw = load_dataset('opus_books', f"{config['lang_src']}-{config['lang_trg']}", split='train')
+    ## Adding final sigmoid layer to U-Net Model
+    sigmoid = 'y'
 
-    # Build tokenizers
-    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
-    tokenizer_trg = get_or_build_tokenizer(config, ds_raw, config['lang_trg'])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-machine', type = str, default = machine, choices=['w', 'm'])
+    parser.add_argument('-execution', type = str, default = execution, choices=['t', 's'], 
+                        help = 't for train, or s for sampling the model')
+    parser.add_argument('-in_channels', type = int, default = in_channels, 
+                        help = '# of channels present in the input data')
+    parser.add_argument('-num_classes', type = int, default = num_classes, 
+                        help = '# of output classes from the U-net model')
+    parser.add_argument('-num_filters', type = int, default = num_filters, 
+                        help = '# of filters you want the first layer of the U-net model to have')
+    parser.add_argument('-training_batch_size', type = int, default = training_batch_size, 
+                        help = 'Batch Size during Training and Validation')
+    parser.add_argument('-training_epochs', type = int, default = training_epochs, 
+                        help = '# of epochs you want to train the model')
+    parser.add_argument('-learning_rate', type = float, default = learning_rate, 
+                        help = 'learning rate')
+    parser.add_argument('-sigmoid_end', type = str, default = sigmoid, choices=['y', 'n'],
+                        help = 'If final layer of Unet should be sigmoid or not (y/n)')
 
-    # Keep 90% for training and rest for validation
-    train_ds_size = int(0.9* len(ds_raw))
-    val_ds_size = len(ds_raw) - train_ds_size
-    train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
-
-    train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_trg, config['lang_src'], config['lang_trg'], config['seq_len'])
-    val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_trg, config['lang_src'], config['lang_trg'], config['seq_len'])
-
-    max_len_src = 0
-    max_len_trg = 0
-
-    for item in ds_raw:
-        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
-        trg_ids = tokenizer_trg.encode(item['translation'][config['lang_trg']]).ids
-        max_len_src = max(max_len_src, len(src_ids))
-        max_len_trg = max(max_len_trg, len(src_ids))
-
-    print(f"Max length of source sentence: {max_len_src}")
-    print(f"Max length of target sentence: {max_len_trg}")
-
-    train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'], shuffle=True)
-    val_dataloader = DataLoader(val_ds, batch_size = 1, shuffle = True)
-
-    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_trg
-
-
-def get_model(config, vocab_src_len, vocab_trg_len):
-    model = build_transformer(vocab_src_len, vocab_trg_len, config['seq_len'], config['seq_len'], config['d_model'])
-    return model
-
-
-def train_model(config):
-
-    # Define the device
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(f"Using Device {device}")
-
-    Path(config['model_folder']).mkdir(parents=True, exist_ok = True)
-
-    train_dataloader, val_dataloader, tokenizer_src, tokenizer_trg = get_ds(config)
-    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_trg.get_vocab_size()).to(device)
-
-    # Tensorboard
-
-    writer = SummaryWriter(config['experiment_name'])
-    
-    optimizer = torch.optim.Adam(model.parameters(), lr = config['lr'], eps = 1E-9)
-
-    initial_epoch = 0
-    global_step = 0
-    if config['preload']:
-        model_filename = get_weights_file_path(config, config['preload'])
-        print(f"Preloading model: {model_filename}")
-        state = torch.load(model_filename)
-        inital_epoch = state['epoch']+1
-        optimizer.load_state_dict(state['optimizer_state_dict'])
-        global_step = state['global_step']
-
-    loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id('[PAD]'), label_smoothing=0.1).to(device)
-
-    for epoch in range(initial_epoch, config['num_epochs']):
-        model.train()
-        batch_iterator = tqdm(train_dataloader, desc=f'Processing epoch {epoch:02d}')
-
-        for batch in batch_iterator:
-            encoder_input = batch['encoder_input'].to(device)   # (b, seq_len)
-            decoder_input = batch['decoder_input'].to(device)   # (b, seq_len)
-            encoder_mask = batch['encoder_mask'].to(device) # (b, 1, 1, seq_len)
-            decoder_mask = batch['decoder_mask'].to(device) # (b, 1, seq_len, seq_len)
-
-            # Run the tensors through the transformer
-            encoder_output = model.encode(encoder_input, encoder_mask)  # (b, seq_len, d_model)
-            decoder_output = model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask)    # (b, seq_len, d_model)
-            proj_output = model.project(decoder_output) # (b, seq_len, trg_vocab_size)
-
-            label = batch['label'].to(device)
-
-            # (b, seq_len, trg_vocab_size) --> (b, seq_len, trg_vocab_size)
-            loss = loss_fn(proj_output.view(-1, tokenizer_trg.get_vocab_size()), label.view(-1))
-            batch_iterator.set_postfix({f"loss": f"{loss.item():6.3f}"})
-
-            # Log the loss
-            writer.add_scalar('train loss', loss.item(), global_step)
-            writer.flush()
-
-            # Backpropagate the loss
-            loss.backward()
-
-            # Update the weights
-            optimizer.step()
-            optimizer.zero_grad()
-
-            global_step += 1
-
-        # Save the model at the end of every epoch
-        model_filename = get_weights_file_path(config, f'{epoch:02d}')
-        torch.save({
-            "epoch": epoch,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'global_step': global_step
-        }, model_filename)
-
-if __name__ == '__main__':
-    warnings.filterwarnings('ignore')
-    config = get_config()
-    train_model(config)
+    args = parser.parse_args()
+    return args
